d1211529ba0682c33074b32c8c1de0bbddb27833
zhangzb <1099055152@qq.com>
2022-02-12 15:21:55 +0800

    主分支提交代码

--- src/main/java/com/uplooking/controller/BookServlet.java
+++ src/main/java/com/uplooking/controller/BookServlet.java
@@ -24,5 +24,6 @@ public class BookServlet extends HttpServlet {
         System.out.println("doGet");
         System.out.println("doGet");
         System.out.println("doGet");
+        System.out.println("doGet");
     }
 }
